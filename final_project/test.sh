850b25c1e1029528665d25cc9e7c982c83d4bd57
kypkk <kypss50102@gmail.com>
2023-01-07 15:36:52 +0800

    MOD: test hidden data

--- final_project/test.sh
+++ final_project/test.sh
@@ -1,20 +1,29 @@
 for i in {1..2}
 do
-echo "---test 01---"
-./smli < public_test_data/01_$i.lsp
-echo "---test 02---"
-./smli < public_test_data/02_$i.lsp
-echo "---test 03---"
-./smli < public_test_data/03_$i.lsp
-echo "---test 04---"
-./smli < public_test_data/04_$i.lsp
-echo "---test 05---"
-./smli < public_test_data/05_$i.lsp
-echo "---test 06---"
-./smli < public_test_data/06_$i.lsp
-echo "---test 07---"
-./smli < public_test_data/07_$i.lsp
-echo "---test 08---"
-./smli < public_test_data/08_$i.lsp
+echo "---test 01_$i---"
+./smli < test_data/01_$i.lsp
+# echo "---test 01_$i hidden---"
+# ./smli < test_data/01_hidden_$i.lsp
+echo "---test 02_$i---"
+./smli < test_data/02_$i.lsp
+# echo "---test 02_$i hidden---"
+# ./smli < test_data/02_hidden_$i.lsp
+echo "---test 03_$i---"
+./smli < test_data/03_$i.lsp
+# echo "---test 03_$i hidden---"
+# ./smli < test_data/03_hidden_$i.lsp
+echo "---test 04_$i---"
+./smli < test_data/04_$i.lsp
+# echo "---test 04_$i hidden---"
+# ./smli < test_data/04_hidden_$i.lsp
+echo "---test 05_$i---"
+./smli < test_data/05_$i.lsp
+# echo "---test 05_$i hidden---"
+# ./smli < test_data/05_hidden_$i.lsp
+echo "---test 06_$i---"
+./smli < test_data/06_$i.lsp
+# echo "---test 06_$i hidden---"
+# ./smli < test_data/06_hidden_$i.lsp
+
 echo " "
 done
